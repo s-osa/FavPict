1c433ce30af77043a93826b417d9344551ba4c02
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-08-27 12:12:43 +0900

    Enable to select image and get description.

--- app/src/main/java/example/android/favpicts/AddPictActivity.java
+++ app/src/main/java/example/android/favpicts/AddPictActivity.java
@@ -1,36 +1,79 @@
 package example.android.favpicts;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 import example.android.favpicts.R;
 
 public class AddPictActivity extends Activity {
+    private static final int REQUEST_PICK_PICT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pict);
+
+        Button selectPictButton = (Button) findViewById(R.id.bt_select_pict);
+        selectPictButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_PICK_PICT);
+            }
+        });
+
+        Button savePictButton = (Button) findViewById(R.id.bt_save);
+        savePictButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(AddPictActivity.this, "Save clicked!", Toast.LENGTH_SHORT).show();
+
+                ImageView imageView = (ImageView) findViewById(R.id.iv_selected_image);
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) imageView.getDrawable();
+                Bitmap image = bitmapDrawable.getBitmap();
+
+                Toast.makeText(AddPictActivity.this, image.toString(), Toast.LENGTH_SHORT).show();
+
+                EditText editText = (EditText) findViewById(R.id.et_description);
+                String description = editText.getText().toString();
+
+                Toast.makeText(AddPictActivity.this, description, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.add_pict, menu);
-        return true;
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_PICK_PICT) {
+                try {
+                    InputStream stream = getContentResolver().openInputStream(data.getData());
+                    Bitmap image = BitmapFactory.decodeStream(stream);
+                    stream.close();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+                    ImageView imageView = (ImageView) findViewById(R.id.iv_selected_image);
+                    imageView.setImageBitmap(image);
+                } catch(Exception e) {
+                }
+            }
+        } else {
+            Toast.makeText(AddPictActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
         }
-        return super.onOptionsItemSelected(item);
     }
 }
